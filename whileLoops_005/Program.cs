db8a7640fd737ccf6ed3b2f57237767137bbda27
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-18 00:21:18 +1100

    First commit

---
+++ whileLoops_005/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace whileLoops_005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How old are you? ");
+            int age = int.Parse(Console.ReadLine());
+            // do while loop is guaranteed to run at
+            // least once
+            do
+            {
+                Console.Write(("Happy Birthday "));
+                // -- = reduce by one
+                age--;
+            } while (age > 0);
+            Console.WriteLine(("\nTaDa!"));
+        }
+    }
+}
